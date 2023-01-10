66c0119dd57066c2f4a8648b972c4c78847e8ffb
Stanley Himawan <sceamerskim@gmail.com>
2023-01-10 20:52:54 +0700

    abcd
    a

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,29 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject Spawn;
+    public int digit;
     private bool hasSpawned = false;
 
     public void ButtonClick()
     {
         if (hasSpawned == false)
         {
+            digit = Random.Range(0, 10);
+            if (digit >=5)
+            {
             SpawnObject(0,0,0);
             hasSpawned = true;
-        }
-        else
-        {
-            
+
+            }
+            else
+            {
+                MoveToScene(0);
+            }
         }
     }
     void Start()
     {
         
-        
+    }
+
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
     }
 
     private void SpawnObject(float posX, float posY, float posZ)
