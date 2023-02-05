f7cce8373dc97088d33dd5f332e78f4a292dea29
Stanley Himawan <sceamerskim@gmail.com>
2023-02-05 12:43:47 +0700

    Update Sesi Foto

--- Assets/Script/AudioButton2.cs
+++ Assets/Script/AudioButton2.cs
@@ -1,23 +1,22 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class AudioButton : MonoBehaviour
+public class AudioButton2 : MonoBehaviour
 {
     public AudioSource audioSource;
     private bool hasSpawned = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PlaySound()
@@ -29,8 +28,8 @@ public class AudioButton : MonoBehaviour
         }
         else
         {
-            SceneManager.LoadScene("HasilFoto");
+            SceneManager.LoadScene("HasilFoto2");
         }
-        
+
     }
 }
